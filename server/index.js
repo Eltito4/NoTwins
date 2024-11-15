2dafed35dc1c51f988a139024b1d6cdbe0a82d3e
Diego <diego.villar.san@gmail.com>
2024-11-15 12:57:22 +0100

    Modify files for netlify url

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ connectDB();
 
 // CORS configuration with proper options
 app.use(cors({
-  origin: ['https://vermillion-smakager-55c20e.netlify.app', 'http://localhost:5173'],
+  origin: ['https://notwins.netlify.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
