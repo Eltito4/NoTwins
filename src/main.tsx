7de6dd30b639ea23a886b2eb8d767e20208a0b18
Amparo Sanchez Martinez <amparosanchezmartinez@MacBook-Pro-de-Amparo.local>
2024-12-13 16:52:27 +0100

    Introducing message system3

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthProvider } from './contexts/AuthContext';
+import { MessageProvider } from './contexts/MessageContext';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
 import './index.css';
@@ -11,8 +12,10 @@ if (!rootElement) throw new Error('Root element not found');
 createRoot(rootElement).render(
   <React.StrictMode>
     <AuthProvider>
-      <App />
-      <Toaster position="bottom-right" />
+      <MessageProvider>
+        <App />
+        <Toaster position="bottom-right" />
+      </MessageProvider>
     </AuthProvider>
   </React.StrictMode>
 );
